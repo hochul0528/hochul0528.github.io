1918b2190bd1665a57430c206cb370e1d75c93d3
hochul0528 <kkm5470@kakao.com>
2026-02-19 12:12:42 +0900

    Add files via upload

--- script_en.js
+++ script_en.js
@@ -1,97 +1,96 @@
 const addons = [
   { 
-    name:"현대 유니버스 2022 올 라인업", 
+    name:"Hyundai 2022 All Line Up", 
     version:"2.0.1", 
     size:"1.2GB",
     date:"2024-10-05",
     redirectUrl:"https://drive.google.com/uc?export=download&id=1jGoRAk9igGogORbJt03Hx9xidbpmbLGW",
-    customButtons:[ { name:"커스텀 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1weipfcj1ZdHgYSzNyGjTAKSg5eo6Jd50" },{ name:"도색 UV 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1iQhqaJy3v2nUTogyj8busET-TtNoxbEg" }],
+    customButtons:[ { name:"Custom Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1weipfcj1ZdHgYSzNyGjTAKSg5eo6Jd50" },{ name:"Paint UV Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1iQhqaJy3v2nUTogyj8busET-TtNoxbEg" }],
     img:"image/universe2022.jpg",
-    manufacturer: "현대",
-    category: "버스"
+    manufacturer: "Hyundai",
+    category: "Bus"
   },
 
   { 
-    name:"기아 그랜버드 실크로드 2022",  
+    name:"Kia Granbird Silkroad 2022",  
     version:"1.4.1", 
     size:"509MB",
     date: "2024-12-15",
     redirectUrl:"https://drive.google.com/uc?export=download&id=1-2miMTCFtdgz5okC3gJVqESjJujsd8U5",
-    customButtons:[ { name:"커스텀 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1sBO-u3zwikbNCkr-qj_VwPSafCmEYfaT" },{ name:"도색 UV 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1ile2Ki4OEiXMLjQnIz_HNRVlaWc3CFXn" }],
+    customButtons:[ { name:"Custom Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1sBO-u3zwikbNCkr-qj_VwPSafCmEYfaT" },{ name:"Paint UV Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1ile2Ki4OEiXMLjQnIz_HNRVlaWc3CFXn" }],
     img:"image/granbird2022.jpg",
-    manufacturer: "기아",
-    category: "버스"
+    manufacturer: "Kia",
+    category: "Bus"
   },
 
   { 
-    name: "현대 유니버스 2017", 
+    name: "Hyundai Universe 2017", 
     version: "1.1.0.2", 
     size: "615MB",
     date: "2024-08-18",
     redirectUrl:"https://drive.google.com/uc?export=download&id=17vx3bVeAPxMNqRhNXMwJdFZqwfnLj-07",
-    customButtons:[ { name:"커스텀 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=12JyrLzKJS-IguUw_E_e3sirQCP32SftG" },{ name:"도색 UV 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1KHwMJWGdnswk6-9iMNUj6T3aHzI8EGvG" }],
+    customButtons:[ { name:"Custom Download", redirectUrl:"https://drive.google.com/uc?export=download&id=12JyrLzKJS-IguUw_E_e3sirQCP32SftG" },{ name:"Paint UV Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1KHwMJWGdnswk6-9iMNUj6T3aHzI8EGvG" }],
     img:"image/universe2017.jpg",
-    manufacturer: "현대",
-    category: "버스"
+    manufacturer: "Hyundai",
+    category: "Bus"
   },
 
   { 
-    name:"기아 뉴그랜버드 실크로드 (구형 그랜버드)", 
+    name:"Kia NewGranbird Silkroad (2014 ~ 2019)", 
     version:"beta. 2.0.2", 
     size:"694MB",
     date: "2026-01-31",
     redirectUrl:"https://drive.google.com/uc?export=download&id=12UFLTM8yDRONBx2Lo8wTetqGWD6Sw9h0",
-    customButtons:[ { name:"커스텀 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=18ToDrJexNwr2IXrcfTUw-VZZVYwnQYJ-" },{ name:"도색 UV 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1vQF-tfrpx5IOQ6Fx_uGymEe6k3p7dMIc" }],
+    customButtons:[ { name:"Custom Download", redirectUrl:"https://drive.google.com/uc?export=download&id=18ToDrJexNwr2IXrcfTUw-VZZVYwnQYJ-" },{ name:"Paint UV Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1vQF-tfrpx5IOQ6Fx_uGymEe6k3p7dMIc" }],
     img:"image/newgranbird.jpg",
-    manufacturer: "기아",
-    category: "버스"
+    manufacturer: "Kia",
+    category: "Bus"
   },
 
   { 
-    name:"현대 일렉시티 2", 
-    version:"beta. 2.0.1", 
-    size:"187MB",
-    date: "2025-05-10",
-    redirectUrl:"https://drive.google.com/uc?export=download&id=1khqs6ZA9Y7PNXHAj33wkrDfjQGILPJsD",
-    customButtons:[ { name:"커스텀 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1k_hmpgS1dBzd_h1AGm1t4-bfsbFX4b-5" },{ name:"도색 UV 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1b8iUvc6U5_Po-ENc-z-raeQNl67HNHYk" }],
-    img:"image/eleccity.jpg",
-    manufacturer: "현대",
-    category: "버스"
-  },
-
-  { 
-    name:"기아 봉고 3", 
+    name:"Kia Bongo 3", 
     version:"1.0", 
     size:"610MB",
     date: "2024-12-03",
     redirectUrl:"https://drive.google.com/uc?export=download&id=1go-0NNvVpauOWYEXj0IbQPHjEFDq52H5",
-    customButtons:[ { name:"커스텀 다운로드", redirectUrl:"https://drive.google.com/uc?export=download&id=1qXHSsRPiJxtnWRiv2GRyRMtNpkQ3viYB" }],
+    customButtons:[ { name:"Custom Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1qXHSsRPiJxtnWRiv2GRyRMtNpkQ3viYB" }],
     img:"image/bongo3.jpg",
-    manufacturer: "기아",
-    category: "트럭"
+    manufacturer: "Kia",
+    category: "Truck"
   },
 
-    { 
-    name:"현대 N Vision 74", 
+  { 
+    name:"Hyundai N Vision 74", 
     version:"1.0", 
     size:"120MB",
     date: "2022-12-04",
     redirectUrl:"https://drive.google.com/uc?export=download&id=1XcczpV5KiSzwZmbBcxY38aDyS-jWvE2t",
     img:"image/vision74.jpg",
-    manufacturer: "기아",
-    category: "스포츠카"
+    manufacturer: "Kia",
+    category: "Car"
   },
 
 
+  { 
+    name:"Hyundai Eleccity 2", 
+    version:"beta. 2.0.1", 
+    size:"187MB",
+    date: "2025-05-10",
+    redirectUrl:"https://drive.google.com/uc?export=download&id=1khqs6ZA9Y7PNXHAj33wkrDfjQGILPJsD",
+    customButtons:[ { name:"Custom Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1k_hmpgS1dBzd_h1AGm1t4-bfsbFX4b-5" },{ name:"Paint UV Download", redirectUrl:"https://drive.google.com/uc?export=download&id=1b8iUvc6U5_Po-ENc-z-raeQNl67HNHYk" }],
+    img:"image/eleccity.jpg",
+    manufacturer: "Hyundai",
+    category: "Bus"
+  },
 
   { 
-    name:"티맵 네비게이션 (창작마당)", 
+    name:"[KR] T Map Voice Navigation (Workshop)", 
     version:"3.01", 
     size:"14MB",
     date: "2022-05-18",
     redirectUrl:"https://steamcommunity.com/sharedfiles/filedetails/?id=2603145129",
     img:"image/tmap.jpg",
-    manufacturer: "티맵",
+    manufacturer: "GPS Sound",
   }
 ];
 
@@ -182,8 +181,8 @@ function displayAddons() {
     card.innerHTML = `
       <img src="${a.img}" alt="${a.name}" />
       <h3>${a.name}</h3>
-      <p>버전: ${a.version} | 용량: ${a.size}</p>
-      <p>업데이트일: ${formatDate(a.date)}</p>
+      <p>Version: ${a.version} | Size: ${a.size}</p>
+      <p>Updated: ${formatDate(a.date)}</p>
     `;
 
     card.onclick = () => openModal(a);
@@ -199,7 +198,7 @@ function openModal(addon) {
 
   modalImg.src = addon.img || "previews/default.png";
   modalName.textContent = addon.name;
-  modalVersion.textContent = `버전: ${addon.version} | 용량: ${addon.size} | 날짜: ${formatDate(addon.date)}`;
+  modalVersion.textContent = `Version: ${addon.version} | Size: ${addon.size} | Updated: ${formatDate(addon.date)}`;
 
   // 기본 다운로드
   modalDownload.onclick = e => {
